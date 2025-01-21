d0dab5e368e5f579e7d1f9e7b3315e148f02e3d0
jayasaptr <info.banjarcode@gmail.com>
2025-01-22 07:15:52 +0800

    fix bug

--- src/Layout/LayoutMenuData.tsx
+++ src/Layout/LayoutMenuData.tsx
@@ -1,14 +1,20 @@
 import {
   BookUser,
   Box,
+  BoxesIcon,
+  ClipboardEditIcon,
   Container,
+  LucideBox,
   Mail,
   MessageSquare,
   MonitorDot,
   PackageCheckIcon,
   PackageMinusIcon,
   PictureInPicture2,
+  PieChart,
   User,
+  UserMinus2,
+  UserPlus2Icon,
 } from "lucide-react";
 
 const menuData: any = [
@@ -72,6 +78,46 @@ const menuData: any = [
     link: "/barang-keluar",
     parentId: 2,
   },
+  {
+    label: "Laporan",
+    isTitle: true,
+  },
+  {
+    id: "laporan_pemasok",
+    label: "Laporan Pemasok",
+    icon: <UserPlus2Icon />,
+    link: "/barang-keluar",
+    parentId: 1,
+  },
+  {
+    id: "laporan_pelanggan",
+    label: "Laporan Pelanggan",
+    icon: <UserMinus2 />,
+    link: "/barang-keluar",
+    parentId: 2,
+  },
+  {
+    id: "mutasi_barang",
+    label: "Laporan Mutasi Barang",
+    icon: <PieChart />,
+    link: "/barang-keluar",
+    parentId: 3,
+  },
+  {
+    id: "barang_tersedia",
+    label: "Laporan Stok Barang Tersedia",
+    icon: <BoxesIcon />,
+    link: "/barang-keluar",
+    parentId: 4,
+  },
+  {
+    id: "barang_habis",
+    label: "Laporan Stok Barang Kosong",
+    icon: <LucideBox />,
+    link: "/barang-keluar",
+    parentId: 5,
+  },
+
 ];
 
 export { menuData };
--- src/pages/MasterUser/index.tsx
+++ src/pages/MasterUser/index.tsx
@@ -85,6 +85,7 @@ const MasterUser = () => {
       if (isEdit) {
         handleUpdateSuratMasuk(values);
       } else {
+        console.log(`user ${values}`);
         handlePostMasterUser(values);
       }
       if (isLoading) {
@@ -360,20 +361,20 @@ const MasterUser = () => {
               item.no = index + 1;
               return item;
             }),
-            (
-              <TableContainer
-                isPagination={true}
-                columns={columns || []}
-                data={data || []}
-                customPageSize={5}
-                divclassName="-mx-5 overflow-x-auto"
-                tableclassName="w-full table-fixed"
-                theadclassName="ltr:text-left rtl:text-right bg-slate-100 dark:bg-zink-600"
-                thclassName="px-3.5 py-2.5 first:pl-5 last:pr-5 font-semibold border-b border-slate-200 dark:border-zink-500"
-                tdclassName="px-3.5 py-2.5 first:pl-5 last:pr-5 border-y border-slate-200 dark:border-zink-500 overflow-hidden text-ellipsis whitespace-nowrap"
-                PaginationClassName="flex flex-col items-center gap-4 px-4 mt-4 md:flex-row"
-              />
-            ))
+              (
+                <TableContainer
+                  isPagination={true}
+                  columns={columns || []}
+                  data={data || []}
+                  customPageSize={5}
+                  divclassName="-mx-5 overflow-x-auto"
+                  tableclassName="w-full table-fixed"
+                  theadclassName="ltr:text-left rtl:text-right bg-slate-100 dark:bg-zink-600"
+                  thclassName="px-3.5 py-2.5 first:pl-5 last:pr-5 font-semibold border-b border-slate-200 dark:border-zink-500"
+                  tdclassName="px-3.5 py-2.5 first:pl-5 last:pr-5 border-y border-slate-200 dark:border-zink-500 overflow-hidden text-ellipsis whitespace-nowrap"
+                  PaginationClassName="flex flex-col items-center gap-4 px-4 mt-4 md:flex-row"
+                />
+              ))
           ) : loadingV ? (
             loadingView
           ) : (
@@ -465,28 +466,26 @@ const MasterUser = () => {
                   <p className="text-red-400">{validation.errors.email}</p>
                 ) : null}
               </div>
-              {isEdit ?? (
-                <div className="xl:col-span-12">
-                  <label
-                    htmlFor="password"
-                    className="inline-block mb-2 text-base font-medium"
-                  >
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
-                    placeholder="Password"
-                    name="password"
-                    onChange={validation.handleChange}
-                    value={validation.values.password || ""}
-                  />
-                  {validation.touched.password && validation.errors.password ? (
-                    <p className="text-red-400">{validation.errors.password}</p>
-                  ) : null}
-                </div>
-              )}
+              {isEdit === false ? <div className="xl:col-span-12">
+                <label
+                  htmlFor="password"
+                  className="inline-block mb-2 text-base font-medium"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
+                  placeholder="Password"
+                  name="password"
+                  onChange={validation.handleChange}
+                  value={validation.values.password || ""}
+                />
+                {validation.touched.password && validation.errors.password ? (
+                  <p className="text-red-400">{validation.errors.password}</p>
+                ) : null}
+              </div> : <></>}
               <div className="xl:col-span-12">
                 <label
                   htmlFor="jabatan"
@@ -524,7 +523,7 @@ const MasterUser = () => {
                   value={validation.values.department || ""}
                 />
                 {validation.touched.department &&
-                validation.errors.department ? (
+                  validation.errors.department ? (
                   <p className="text-red-400">{validation.errors.department}</p>
                 ) : null}
               </div>
@@ -571,8 +570,8 @@ const MasterUser = () => {
                 {isLoading
                   ? "Loading"
                   : !!isEdit
-                  ? "Update"
-                  : "Add Master User"}
+                    ? "Update"
+                    : "Add Master User"}
               </button>
             </div>
           </form>
